86349b188c46b60126fdc91627c51ee8f8662e4b
isaac meira <43460570+isaacmeira@users.noreply.github.com>
2019-08-11 19:04:30 -0300

    Added UserCreation (#22)
    * Added UserCreation
    
    Addeded a function for create user and remove some innutil things
    
    * hotfix

--- src/database/migrations/20190729143455-create-users.js
+++ src/database/migrations/20190729143455-create-users.js
@@ -2,22 +2,63 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+      full_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      social_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      gender: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      date_of_birth: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
 };
